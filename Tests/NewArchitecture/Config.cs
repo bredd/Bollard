96fc217665c2ea385e1089ba364b9af49b16ca20
Brandt Redd <brandt@redd.org>
2026-04-06 09:24:04 -0600

    Additional testing.

--- Tests/NewArchitecture/Config.cs
+++ Tests/NewArchitecture/Config.cs
@@ -16,6 +16,7 @@ Trace.WriteLine("Sample trace message.");
 Console.WriteLine($"Greeting = {greeting}");
 Console.WriteLine($"Site.Test = {Site.Test}");
 Console.WriteLine($"Can you {(TestTarget.AllCaps("Shout"))}?");
+Console.WriteLine($"3 * 5 = {Multiply.Times(3, 5)}");
 Console.WriteLine("Config.cs test succeeded.");
 Console.WriteLine();
 return 0; // Exit the launch function
---
+++ Tests/NewArchitecture/Multiply.cs
@@ -0,0 +1,5 @@
+internal static class Multiply {
+    public static int Times(int x, int y) {
+        return x * y;
+    }
+ }
